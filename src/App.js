9a7846581d81ca2240f00288c8d973c4ee50a8db
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-10-09 22:12:57 -0400

    basic functionality done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, createRef } from 'react';
 import './App.css';
 import axios from "axios";
-import styled, { Select, keyframes } from "styled-components"
-
+import styled, { keyframes } from "styled-components"
 import Character from "./components/Character"
 import Details from "./components/Details"
+import Select from "react-select"
+const { v4: uuidv4 } = require('uuid');
 
-const BASE_URL = "https://swapi.dev/api/"
 
-const keyframeAnimation = keyframes`
-  25% {
-    opacity: 10%;
-    transform: scale(0.1);
-    transform: rotateX(180deg);
-    transform: rotateY(90deg);
-  }
-  50% {
-    opacity: 40%;
-    transform: scale(0.4);
-    transform: rotateX(180deg);
-    transform: rotateY(90deg);
-  }
-  75% {
-    opacity: 80%;
-    transform: scale(0.7);
-    transform: rotateX(180deg);
-    transform: rotateY(90deg);
-  }
-  99% {
-    opacity: 100%;
-    transform: scale(0.9);
-    transform: rotateX(180deg);
-    transform: rotateY(90deg);
-  }
+export const BASE_URL = "https://swapi.dev/api/people/"
+
+const StyledSelect = styled(Select)`
+  width: 25%;
+  margin: 25px 0;
 `
 
-const StyledDiv = styled.div`
+const StyledDiv = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* border: 1px solid red; */
-  transform: scale(1);
-  opacity: 100%;
-  animation: ${keyframeAnimation} 0.9s ease-in-out;
 `
+export const ids = {
+  "Luke Skywalker": 1,
+  "C-3PO": 2,
+  "R2-D2": 3,
+  "Darth Vader": 4,
+  "Leia Organa": 5,
+  "Owen Lars": 6,
+  "Beru Whitesun lars": 7,
+  "R5-D4": 8,
+  "Biggs Darklighter": 9,
+  "Obi-Wan Kenobi": 10
+}
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -53,52 +41,38 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const [characters, setCharacters] = useState([])
-  const [character, setCharacter] = useState(null)
-  const selectRef = useRef()
+  const [charName, setCharName] = useState(null)
+  // const selectRef = createRef()
 
   useEffect(() => {
-    axios.get(`${BASE_URL}people/`)
+    axios.get(`${BASE_URL}`)
       .then(resp => resp.data.results)
       .then(chars => setCharacters(chars))
       .catch(err => { debugger })
   }, [])
 
-  const selectChar = (e) => {
-    e.preventDefault()
-    console.log(e)
-    // console.log(selectRef.current.value)
-  }
+  const fillSelect = characters.map(char => {
+      return {
+        "value" : char.name,
+        "label" : char.name
+      }
+  })
 
   return (
-    <div>
-        {/* {characters.length !== 0 
-          ? <select ref={selectRef} onChange={selectChar} name="characters" id="chars">
-              <option value={characters[0]}>{characters[0].name}</option>
-              <option value={characters[1]}>{characters[1].name}</option> 
-              <option value={characters[2]}>{characters[2].name}</option>
-              <option value={characters[3]}>{characters[3].name}</option>
-              <option value={characters[4]}>{characters[4].name}</option>
-              <option value={characters[5]}>{characters[5].name}</option>
-              <option value={characters[6]}>{characters[6].name}</option>
-              <option value={characters[7]}>{characters[7].name}</option>
-              <option value={characters[8]}>{characters[8].name}</option>
-              <option value={characters[9]}>{characters[9].name}</option>
-            </select>
-          <select 
-        name="form-field-name"
-        placeholder="Select a brand"
-        searchable={false}
-        value={selectedOption}
-        onChange={handleChange}
-        options={options}
-      />
-          : null
-        }
-        {characters.map(char => {
-
-        })} */}
-
-    </div>
+    <StyledDiv className="App">
+      <h1 className="Header">Choose a Star Wars character:</h1>
+      {
+        characters &&
+          <StyledSelect onChange={ e => setCharName(e.value) } options={ fillSelect } name={ characters } id="chooseChar" />
+          // <StyledForm ref={ formRef }>
+          //   <Select options={ getChars } />
+          // </StyledForm>
+      }
+      {
+        charName !== null &&
+          <Character char={ charName } />
+      }
+    </StyledDiv>
   );
 }
 
@@ -113,9 +87,6 @@ export default App;
 //   // sync up with, if any.
 
 //   return (
-//     <div className="App">
-//       <h1 className="Header">Characters</h1>
-//     </div>
 //   );
 // }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,55 +1,62 @@
 // Write your Character component here
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
-import { Button } from "reactstrap"
-
-import { StyledBtn } from "./Details"
+import axios from "axios"
+import { ids, BASE_URL } from "../App"
 
 const StyledDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border: 3px solid black;
-    border-radius: 10px;
-    box-shadow: 10px 10px 0 0;
-    width: 60%;
-    margin: 10px auto;
+    width: 50%;
     background-color: ${props => props.theme.secondaryColor};
-    /* color: ${props => props.theme.primaryColor}; */
-    /* justify-content: center; */
-    h3 {
-        color: ${props => props.theme.tertiaryColor};
-    }
+    padding: 10px 20px;
+    border: 1px solid blue;
 `
 
-const StyledSubDiv = styled.div`
+const StyledChild = styled.div`
     display: flex;
-    width: 60%;
+    justify-content: space-evenly;
     border: 1px solid red;
 `
 
-const StyledUL = styled.ul`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    color: ${props => props.theme.primaryColor};
-    width: 50%;
-    border: 1px solid black;
+const StyledP = styled.p`
+
 `
 
-const StyledP = styled.p`
-    font-size: 1.2rem;
-    ${props => (props.type === "key" ? "color: #2D080A;" : null)};
-    ${props => (props.type === "value" ? "color: #72705B;" : null)};
+const StyledUL = styled.ul`
+
 `
 
 export default function Character(props) {
-    const { person, open } = props
+    const { char } = props
+    const [info, setInfo] = useState(null)
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}${ids[char]}`)
+            .then(resp => setInfo(resp.data))
+            .catch(err => console.log(err))
+    }, [char])
 
     return (
-        <StyledDiv>
-            <h3>{ person.name }</h3>
-            <StyledBtn type="open" onClick={ () => open(person.name) }>See Details</StyledBtn>
-        </StyledDiv>
+        // <StyledDiv>
+        //     <h3>{ person.name }</h3>
+        //     <StyledBtn type="open" onClick={ () => open(person.name) }>See Details</StyledBtn>
+        // </StyledDiv>
+        info && 
+            <StyledDiv>
+                <h3>{ info.name }</h3>
+                <StyledChild>
+                    <>
+                        <StyledUL>
+                            <li><StyledP type="key">Height:  </StyledP> <StyledP type="value">{ info.height } cm</StyledP></li>
+                            <li><StyledP type="key">Mass:  </StyledP> <StyledP type="value">{ info.mass } kg</StyledP></li>
+                            <li><StyledP type="key">Hair Color:  </StyledP> <StyledP type="value">{ info.hair_color}</StyledP></li>
+                            </StyledUL>
+                            <StyledUL>
+                            <li><StyledP type="key">Skin Color:  </StyledP> <StyledP type="value">{ info.skin_color}</StyledP></li>
+                            <li><StyledP type="key">Eye Color:  </StyledP> <StyledP type="value">{ info.eye_color}</StyledP></li>
+                            <li><StyledP type="key">Gender:  </StyledP> <StyledP type="value">{ info.gender }</StyledP></li>
+                        </StyledUL>
+                    </>
+                </StyledChild>
+            </StyledDiv>
     )
 }
